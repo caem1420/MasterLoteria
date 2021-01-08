02c7b541e7a153c0b322d3380fe6923b40d79c62
Carlos Escobar <ecarlosandres@gmail.com>
2021-01-08 15:06:42 -0500

    0b

---
+++ MasterLoteria/Tabs/SuerteView.swift
@@ -0,0 +1,20 @@
+//
+//  SuerteView.swift
+//  MasterLoteria
+//
+//  Created by Carlos Escobar on 8/01/21.
+//
+
+import SwiftUI
+
+struct SuerteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SuerteView_Previews: PreviewProvider {
+    static var previews: some View {
+        SuerteView()
+    }
+}
---
+++ MasterLoteria/Tabs/TabAyerView.swift
@@ -0,0 +1,20 @@
+//
+//  TabAyerView.swift
+//  MasterLoteria
+//
+//  Created by Carlos Escobar on 8/01/21.
+//
+
+import SwiftUI
+
+struct TabAyerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TabAyerView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabAyerView()
+    }
+}
