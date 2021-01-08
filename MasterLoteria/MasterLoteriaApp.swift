f43dfba32dd184a59141e855984cda4e80feac7f
Carlos Escobar <ecarlosandres@gmail.com>
2021-01-08 11:06:25 -0500

    Initial Commit

---
+++ MasterLoteria/MasterLoteriaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasterLoteriaApp.swift
+//  MasterLoteria
+//
+//  Created by Carlos Escobar on 8/01/21.
+//
+
+import SwiftUI
+
+@main
+struct MasterLoteriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
